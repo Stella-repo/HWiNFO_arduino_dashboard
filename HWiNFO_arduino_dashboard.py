3f9d9d2c04b546854c1f384890eb4bc5e8f58268
Stella-repo <93361308+Stella-repo@users.noreply.github.com>
2021-10-31 01:41:06 +0900

    modify code to pretty

--- HWiNFO_arduino_dashboard.py
+++ HWiNFO_arduino_dashboard.py
@@ -5,18 +5,16 @@ import serial
 import threading
 from datetime import datetime
 import time
-import threading
 
 ser = serial.Serial(port='COM3', baudrate=9600)
 
 def hwinfo():
-	
 	#################### 디스크 남은량 ####################
 	cdr_abl = str(math.floor(psutil.disk_usage('c:')[2]/2.**30))
 	ddr_abl = str(math.floor(psutil.disk_usage('d:')[2]/2.**30))
 	#################### 디스크 남은량 ####################
 	
-	#################### HWiufo 레지스트리값 ####################
+	#################### HWiNFO 레지스트리값 ####################
 	reg = os.popen('reg query HKEY_CURRENT_USER\SOFTWARE\HWiNFO64\VSB').readlines()
 	mem_abl = str(reg[3][24:reg[3].rfind('GB')-1])
 	mem_lod = str(math.ceil(float(reg[6][24:reg[6].rfind('%')-1])))
@@ -46,17 +44,16 @@ def hwinfo():
 
 	int_dlr = float(reg[66][25:reg[66].rfind('Kbps')-1])
 	int_upr = float(reg[69][25:reg[69].rfind('Kbps')-1])
-	#################### HWiufo 레지스트리값 ####################
+	#################### HWiNFO 레지스트리값 ####################
 	
-	
-	#################### GPU 최고 사용량 계산 ####################
+	#################### GPU 최고 사용량 판단 및 값 반올림 ####################
 	gpu_usg = []
 	gpu_usg.extend([gpu_cor, gpu_vid, gpu_d3d, gpu_dec, gpu_enc])
 	max_gpu_usg = math.ceil((max(gpu_usg)))
 	gpu_enc = math.ceil(gpu_enc)
-	#################### GPU 최고 사용량 계산 ####################
+	#################### GPU 최고 사용량 판단 및 값 반올림 ####################
 	
-	#################### 디스크, 인터넷 사용량 단위 ####################
+	#################### 디스크, 인터넷 사용량 및 단위 ####################
 	##D드라이브
 	if ddr_red < 999:
 		ddr_red_unit = 'K'
@@ -115,7 +112,7 @@ def hwinfo():
 		else:
 			cdr_wrt = str(math.ceil(cdr_wrt))
 			
-	##C인터넷
+	##인터넷
 	if int_upr < 999:
 		int_upr_unit = 'K'
 		if int_upr < 100:
@@ -143,13 +140,14 @@ def hwinfo():
 			int_dlr = str(round(int_dlr, 1))
 		else:
 			int_dlr = str(math.ceil(int_dlr))
-	#################### 디스크, 인터넷 사용량 단위 ####################
+	#################### 디스크, 인터넷 사용량 및 단위 ####################
 	
 	send_data = str(mem_abl) + '/' + str(mem_lod) + '/' + str(cpu_usg) + '/' + str(cpu_tmp) + '/' + str(cpu_fan) + '/' + str(fnt_fan) + '/' + str(t_sen) + '/' + str(ddr_act) + '/' + str(ddr_red) + '/' + str(ddr_wrt) + '/' + str(ddr_red_unit) + '/' + str(ddr_wrt_unit) + '/' + str(cdr_act) + '/' + str(cdr_red) + '/' + str(cdr_wrt) + '/' + str(cdr_red_unit) + '/' + str(cdr_wrt_unit) +'/' + str(gpu_tmp) + '/' + str(gpu_fan) + '/' + str(max_gpu_usg) +'/' + str(gpu_enc) + '/' + str(int_upr) + '/' + str(int_dlr) + '/' + str(int_upr_unit)+ '/' +  str(int_dlr_unit) + '/' + str(cdr_abl) + '/' + str(ddr_abl) + '/' + '>'
 	print(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
 	print(send_data.encode())
 	
 	ser.write(send_data.encode())
+
 
 while True:
 	threading.Thread(target=hwinfo).start()
