62cd3d0cdf5bb42e6795998151242a8248a5ef61
Stella-repo <93361308+Stella-repo@users.noreply.github.com>
2021-11-03 00:29:31 +0900

    Add remark

--- HWiNFO_arduino_dashboard.py
+++ HWiNFO_arduino_dashboard.py
@@ -6,7 +6,7 @@ import threading
 from datetime import datetime
 import time
 
-ser = serial.Serial(port='COM3', baudrate=9600)
+ser = serial.Serial(port='COM3', baudrate=9600) #아두이노와 연결된 포트에 맞게 수정하기
 
 def hwinfo():
 	#################### 디스크 남은량 ####################
@@ -15,9 +15,9 @@ def hwinfo():
 	#################### 디스크 남은량 ####################
 	
 	#################### HWiNFO 레지스트리값 ####################
-	reg = os.popen('reg query HKEY_CURRENT_USER\SOFTWARE\HWiNFO64\VSB').readlines()
-	mem_abl = str(reg[3][24:reg[3].rfind('GB')-1])
-	mem_lod = str(math.ceil(float(reg[6][24:reg[6].rfind('%')-1])))
+	reg = os.popen('reg query HKEY_CURRENT_USER\SOFTWARE\HWiNFO64\VSB').readlines() #레지스트리로 센서값 불러오기
+	mem_usg = str(reg[3][24:reg[3].rfind('GB')-1]) #불러온 레지스트리값의 3번째 줄에서 24번째 글자부터 GB글자 전까지 문자열 잘라내기
+	mem_lod = str(math.ceil(float(reg[6][24:reg[6].rfind('%')-1]))) #문자열 편집후 실수로 소수점 둘쨰자리에서 변환하고 올림하기
 
 	cpu_usg = str(math.ceil(float(reg[9][24:reg[9].rfind('%')-1])))
 	cpu_tmp = str(reg[12][24:reg[12].rfind('°C')-1])
@@ -50,17 +50,18 @@ def hwinfo():
 	gpu_usg = []
 	gpu_usg.extend([gpu_cor, gpu_vid, gpu_d3d, gpu_dec, gpu_enc])
 	max_gpu_usg = math.ceil((max(gpu_usg)))
+		#윈도우에서는 GPU의 Core, Video Engine, D3D, Video Decode, Video Encode 로드율 중에서 제일 큰 값을 출쳑함
 	gpu_enc = math.ceil(gpu_enc)
 	#################### GPU 최고 사용량 판단 및 값 반올림 ####################
 	
 	#################### 디스크, 인터넷 사용량 및 단위 ####################
 	##D드라이브
-	if ddr_red < 999:
+	if ddr_red < 999: #999넘는 숫자는 올림하면 1000이 되기때문에 999보다 작은수를 지정함
 		ddr_red_unit = 'K'
 		if ddr_red < 100:
-			ddr_red = str(ddr_red)
+			ddr_red = str(ddr_red) #100보다 작으면 소수점 첫째자리까지 표시
 		else:
-			ddr_red = str(math.ceil(ddr_red))		
+			ddr_red = str(math.ceil(ddr_red)) #100이상이면 소수점 표시 안함
 	else:
 		ddr_red = ddr_red/1000
 		ddr_red_unit = 'M'
@@ -142,13 +143,13 @@ def hwinfo():
 			int_dlr = str(math.ceil(int_dlr))
 	#################### 디스크, 인터넷 사용량 및 단위 ####################
 	
-	send_data = str(mem_abl) + '/' + str(mem_lod) + '/' + str(cpu_usg) + '/' + str(cpu_tmp) + '/' + str(cpu_fan) + '/' + str(fnt_fan) + '/' + str(t_sen) + '/' + str(ddr_act) + '/' + str(ddr_red) + '/' + str(ddr_wrt) + '/' + str(ddr_red_unit) + '/' + str(ddr_wrt_unit) + '/' + str(cdr_act) + '/' + str(cdr_red) + '/' + str(cdr_wrt) + '/' + str(cdr_red_unit) + '/' + str(cdr_wrt_unit) +'/' + str(gpu_tmp) + '/' + str(gpu_fan) + '/' + str(max_gpu_usg) +'/' + str(gpu_enc) + '/' + str(int_upr) + '/' + str(int_dlr) + '/' + str(int_upr_unit)+ '/' +  str(int_dlr_unit) + '/' + str(cdr_abl) + '/' + str(ddr_abl) + '/' + '>'
+	send_data = str(mem_usg) + '/' + str(mem_lod) + '/' + str(cpu_usg) + '/' + str(cpu_tmp) + '/' + str(cpu_fan) + '/' + str(fnt_fan) + '/' + str(t_sen) + '/' + str(ddr_act) + '/' + str(ddr_red) + '/' + str(ddr_wrt) + '/' + str(ddr_red_unit) + '/' + str(ddr_wrt_unit) + '/' + str(cdr_act) + '/' + str(cdr_red) + '/' + str(cdr_wrt) + '/' + str(cdr_red_unit) + '/' + str(cdr_wrt_unit) +'/' + str(gpu_tmp) + '/' + str(gpu_fan) + '/' + str(max_gpu_usg) +'/' + str(gpu_enc) + '/' + str(int_upr) + '/' + str(int_dlr) + '/' + str(int_upr_unit)+ '/' +  str(int_dlr_unit) + '/' + str(cdr_abl) + '/' + str(ddr_abl) + '/' + '>'
 	print(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
 	print(send_data.encode())
 	
-	ser.write(send_data.encode())
+	ser.write(send_data.encode()) #아두이노로 전송
 
 
 while True:
-	threading.Thread(target=hwinfo).start()
+	threading.Thread(target=hwinfo).start() #쓰레딩 안하고 그냥 1초쉬면 코드 실행시간까지 겹쳐서 재대로 1초마다 실행이 안됨
 	time.sleep(1)
